6985735913606f47a6205f6f04d78a4f8d148f88
Christian Curilla <C.Curilla@funk-gruppe.de>
2024-03-23 16:56:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleKafkaSaslClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
