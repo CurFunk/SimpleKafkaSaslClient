9ff3bbb5b3a97948e3ed57ed8f2844dd9eed33ae
Christian Curilla <C.Curilla@funk-gruppe.de>
2024-03-23 17:28:36 +0100

    Erste Version.

---
+++ Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleKafkaSaslClient
+{
+    public class Options
+    {
+        [Option(Required = true, HelpText = "Adresse und Port des Kafka Brokers.")]
+        public string? BootstrapServers { get; set; }
+
+        [Option(Required = true, HelpText = "Gruppen-ID des Consumers.")]
+        public string? GroupId { get; set; }
+
+        [Option(Required = true, HelpText = " Benutzername für die SASL-Authentifizierung.")]
+        public string? SaslUsername { get; set; }
+
+        [Option(Required = true, HelpText = "Passwort für die SASL-Authentifizierung.")]
+        public string? SaslPassword { get; set; }
+
+        [Option(Required = true, HelpText = "Topic, dem der Consumer beitreten soll.")]
+        public string? Topic { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,67 @@
-﻿namespace SimpleKafkaSaslClient
+﻿using CommandLine;
+using Confluent.Kafka;
+
+namespace SimpleKafkaSaslClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await Parser.Default.ParseArguments<Options>(args).WithParsedAsync<Options>(async (Options opt) =>
+            {
+                var cancellationTokenSource = new CancellationTokenSource();
+                Console.CancelKeyPress += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Kafka: Verbindung wird beendet.");
+                    // Bei einer Tastenunterbrechung (z.B. CTRL+C) die Cancellation durchführen
+                    eventArgs.Cancel = true; // Verhindert, dass das Programm beendet wird
+                    cancellationTokenSource.Cancel();
+                };
+
+                var config = new ConsumerConfig
+                {
+                    BootstrapServers = opt.BootstrapServers, 
+                    GroupId = opt.GroupId,
+                    AutoOffsetReset = AutoOffsetReset.Earliest, // Offset-Einstellung für den Consumer
+                    SecurityProtocol = SecurityProtocol.SaslSsl, // Sicherheitsprotokoll
+                    SaslMechanism = SaslMechanism.Plain, // Mechanismus für die Authentifizierung (z.B. Plain, ScramSha256, etc.)
+                    SaslUsername = opt.SaslUsername, 
+                    SaslPassword = opt.SaslPassword,
+                };
+
+                try
+                {
+                    using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                    {
+                        Console.WriteLine($"Kafka: Es wird das Topic '{opt.Topic}' abonniert.");
+                        consumer.Subscribe(opt.Topic);
+
+                        try
+                        {
+                            while (!cancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                var consumeResult = consumer.Consume(cancellationTokenSource.Token); // Daten vom Kafka-Cluster abrufen
+                                Console.WriteLine($"Kafka: Nachricht erhalten '{consumeResult.Message.Value}'"); // Nachricht verarbeiten
+                            }
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            // Bei Bedarf aufgefangene Ausnahme
+                        }
+                        finally
+                        {
+                            consumer.Close(); // Consumer schließen, wenn er nicht mehr benötigt wird
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    Environment.Exit(-1);
+                }
+            });
+            Console.WriteLine("Anwendung wird beendet.");
+            return 0;
         }
     }
 }
